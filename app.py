3f37e2fca8f8434b29d2a33edd8279a591163874
roolook <160279839+roolook@users.noreply.github.com>
2025-06-11 12:58:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -47,5 +47,8 @@ def yt_to_text():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
